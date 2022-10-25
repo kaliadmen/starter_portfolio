5777cf04b7d6d9a27c0787f6ac9eb10f150c1672
kaliadmen <kaliadmen@gmail.com>
2022-10-25 13:07:36 -0400

    ADD Header animation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,11 +7,10 @@ const Header = () => {
   return (
       <header>
         <div className="container header__container">
-          <h5>Hi I'm</h5>
-          <h1>Brandon Worthy</h1>
-          <h5 className="text-light">
+          <h2>Brandon Worthy</h2>
+          <h1 className="text-light">
             Fullstack Developer
-          </h5>
+          </h1>
           {/*  call to action*/}
           <CTA/>
 
