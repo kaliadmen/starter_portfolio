930bf14fe2b3bcc005d661833a6868be926f4e79
kaliadmen <kaliadmen@gmail.com>
2022-10-27 14:20:09 -0400

    ADD Navbar and about section content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,51 @@
 import './about.css';
+import AboutImage from "../../assets/me-about.jpg";
+import {FaAward} from "react-icons/fa";
+import {FiUser} from "react-icons/fi";
+import {VscFolderLibrary} from "react-icons/vsc";
 
 const About = () => {
   return (
-      <div>
-        About
-      </div>
+      <section id="about">
+        <h5>Here's more</h5>
+        <h2>About Me</h2>
+        <div className="container about__container">
+          <div className="about__me">
+            <div className="about__image">
+              <img src={AboutImage} alt="about me"/>
+            </div>
+          </div>
+          <div className="about__content">
+              <div className="about__cards">
+                <article className="about__card">
+                  <FaAward className="about__icon"/>
+                  <h5>Experience</h5>
+                  <small>3 + years</small>
+                </article>
+
+                <article className="about__card">
+                  <FiUser className="about__icon"/>
+                  <h5>Clients</h5>
+                  <small>3</small>
+                </article>
+
+                <article className="about__card">
+                  <VscFolderLibrary className="about__icon"/>
+                  <h5>Projects</h5>
+                  <small>5 completed</small>
+                </article>
+              </div>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Deserunt, dicta, illum! Aperiam dolor ipsa ipsam minima nihil
+              non porro quibusdam quis repellat repellendus sunt, tempore
+              totam velit voluptas voluptatum? Molestias.</p>
+
+            <a href="#contact" className="btn btn-primary">Get in Touch</a>
+            </div>
+
+        </div>
+      </section>
   );
 };
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,21 @@
 import './navbar.css';
+import {AiOutlineHome, AiOutlineUser} from "react-icons/ai";
+import {BiBook, BiMessageDots} from "react-icons/bi";
+import {useState} from "react";
+import {BsTools} from "react-icons/bs";
+
 
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState("home");
+
   return (
-      <div>
-        Nav
-      </div>
+      <nav>
+        <a href="#home" onClick={() => setActiveNav("#home")} className={activeNav === "#home " ? "active": ""}><AiOutlineHome/></a>
+        <a href="#about" onClick={() => setActiveNav("#about")} className={activeNav === "#about" ? "active": ""}><AiOutlineUser/></a>
+        <a href="#experience" onClick={() => setActiveNav("#experience")} className={activeNav === "#experience" ? "active": ""}><BiBook/></a>
+        <a href="#services" onClick={() => setActiveNav("#services")} className={activeNav === "#services" ? "active": ""}><BsTools/></a>
+        <a href="#contact" onClick={() => setActiveNav("#contact")} className={activeNav === "#contact" ? "active": ""}><BiMessageDots/></a>
+      </nav>
   );
 };
 
