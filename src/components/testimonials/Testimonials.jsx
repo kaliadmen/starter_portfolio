57062641bef480bfed7366c9597db2ad6e79ce3a
kaliadmen <kaliadmen@gmail.com>
2022-11-03 15:05:03 -0400

    ADD Testimonials section content

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,42 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+
 import './testimonials.css';
+import {data} from "../../reviews";
 
 const Testimonials = () => {
   return (
-      <div>
-        Testimonials
-      </div>
+      <section id="testimonials">
+        <h5>What others have said</h5>
+        <h2>Testimonials</h2>
+
+        <Swiper
+            pagination={{
+              dynamicBullets: true,
+            }}
+            modules={[Pagination]}
+            className="container testimonials__container">
+        {
+          data.map(({id, avatar, name, review}) => {
+            return (
+                  <SwiperSlide className="testimonial" key={id}>
+                    <div className="client__avatar">
+                      <img src={avatar} alt="client avatar"/>
+                    </div>
+                    <h5 className="client__name">
+                      {name}
+                    </h5>
+                    <small className="client__review">
+                      {review}
+                    </small>
+                  </SwiperSlide>
+            )
+          })
+        }
+        </Swiper>
+      </section>
   );
 };
 
