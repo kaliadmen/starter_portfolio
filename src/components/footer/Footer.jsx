40218ececfcf8222082ef3e42b59f050ebd805f8
kaliadmen <kaliadmen@gmail.com>
2022-11-03 17:28:47 -0400

    ADD Footer section content

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,31 @@
 import './footer.css';
+import {BsLinkedin} from "react-icons/bs";
+import {FaGithub} from "react-icons/fa";
 
 const Footer = () => {
   return (
-      <div>
-        Footer
-      </div>
+      <footer>
+        <a href="#" className="footer__logo">WORTHY</a>
+
+        <ul className="permalinks">
+          <li className="permalink"><a href="#">Home</a></li>
+          <li className="permalink"><a href="#about">About</a></li>
+          <li className="permalink"><a href="#experince">Experience</a></li>
+          <li className="permalink"><a href="#services">Services</a></li>
+          <li className="permalink"><a href="#portfolio">Portfolio</a></li>
+          <li className="permalink"><a href="#testimonials">Testimonials</a></li>
+          <li className="permalink"><a href="#contact">Contact</a></li>
+        </ul>
+
+        <div className="footer__socials">
+          <a href="https://www.linkedin.com/in/brandon-worthy-3b093712a" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+          <a href="https://github.com/kaliadmen" target="_blank" rel="noreferrer"><FaGithub/></a>
+        </div>
+
+        <div className="footer__copyright">
+          <small>&copy; WORTHY All rights reserved</small>
+        </div>
+      </footer>
   );
 };
 
